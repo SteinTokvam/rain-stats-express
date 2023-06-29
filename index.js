5de03fb7ac263833f2ffa4664cfceea52cb53495
Stein Petter Tokvam <stto1210@gmail.com>
2023-06-29 16:21:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ import cors from "cors";
 const app = express();
 dotenv.config()
 app.use(cors())
-console.log(process.env.REACT_APP_FIREBASE_DATABASE_URL)
+console.log(process.env.FIREBASE_DATABASE_URL)
 
     async function getRain() {
         const firebaseConfig = {
             apiKey: process.env.FIREBASE_API_KEY,
-            authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-            databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-            projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-            storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-            messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDING_ID,
-            appId: process.env.REACT_APP_FIREBASE_APP_ID,
-            measurementId: process.env.REACT_APP_FIREBASE_MEASSUREMENT_ID
+            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+            databaseURL: process.env.FIREBASE_DATABASE_URL,
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.FIREBASE_MESSAGING_SENDING_ID,
+            appId: process.env.FIREBASE_APP_ID,
+            measurementId: process.env.FIREBASE_MEASSUREMENT_ID
           };
           
         const firebaseApp = initializeApp(firebaseConfig);
@@ -30,7 +30,6 @@ console.log(process.env.REACT_APP_FIREBASE_DATABASE_URL)
             const data = snapshot.val()
             const keys = Object.keys(data)
             const ret = []
-            console.log("inne i snapshot")
             keys.forEach(function(key) {
                 ret.push(data[key])
             })
